70f8e7b41d75d53783d48f0d71491868ed2c7240
Adithya Anilkumar <adithyaanilkumar1@gmail.com>
2023-01-04 23:51:25 +0530

    Update ss.sh

--- ss.sh
+++ ss.sh
@@ -2,20 +2,21 @@
 
 echo "
 
----- COMPILER DESIGN ----
+---- SYSTEM SOFTWARE ----
 
-1. LEXICAL ANALYZER USING C
-2. LEXICAL ANALYZER USING LEX
-3. DISPLAY NUMBER OF LINES, WORDS AND CHARACTERS USING LEX
-4. LEX PROGRAM TO CONVERT abc to ABC
+1. Absolute Loader
+2. Assembler Pass 1
+3. Assembler Pass 2
 
-5. YACC SPECIFICATION TO RECOGONIZE VALID ARITHMETIC EXP
-6. YACC SPECIFICATION TO RECOGONIZE VALID IDENTIFIER
-7. IMPLEMENTATION OF CALCULATOR USING LEX AND YACC
-8. BNF RULES INTO YACC FORM AND GENERATE ABSTRACT SYNTAX TREE
-9. EPSILON CLOSURE OF ALL STATES IN GIVEN NFA WITH EPSILON TRANSITION
+4. Index File Allocation
+5. Linked file Allocation
+6. Sequential File Allocation
 
-10. NFA WITH EPSILON TRANSITION TO NFA WITHOUT EPSILON TRANSITION
+7. Paging
+8. Single Level
+9. Two Level
+
+10. Loader Pass 1
 11. NFA TO DFA
 12. MINIMIZE DFA
 13. FIND FIRST AND FOLLOW OF GRAMMAR
@@ -36,81 +37,76 @@ echo -e "\n\n"
 
 if [[ choice -eq 1 ]]
 then 
-    curl -s https://raw.githubusercontent.com/nosomnesanates/CD_Lab_S7/master/Lexical_Analyzer/lexical_analyzer.c --output lexical_analyzer.c
-    echo "lexical_analyzer.c"
+    curl -s https://raw.githubusercontent.com/alanmathews9/s5-ss-lab/main/alan%20cs5a/absloader.c --output absolute_loader.c
+    curl -s https://github.com/alanmathews9/s5-ss-lab/raw/main/alan%20cs5a/input.dat --output input.dat
+    curl -s https://github.com/alanmathews9/s5-ss-lab/raw/main/alan%20cs5a/output.dat --output output.dat
+    echo "copied to file absolute_loader.c "
+    echo "copied input.dat to file input.dat"
+    echo "copied output.dat to file output.dat"
+    echo "in the present working directory ( current folder )"
+    echo "CHANGE THE CONTENTS OF ALL TXT FILES ACCORDING TO THE QUESTION"
 elif [[ choice -eq 2 ]]
 then
- curl -s https://raw.githubusercontent.com/nosomnesanates/CD_Lab_S7/master/Lexical_Analyzer_Using_Lex/lexical_analyzer.lex --output lexical_analyzer.lex
- echo "lexical_analyzer.lex"
+ curl -s https://raw.githubusercontent.com/alanmathews9/s5-ss-lab/main/alan%20cs5a/pass1.c --output pass_1.c
+ curl -s https://raw.githubusercontent.com/alanmathews9/s5-ss-lab/main/alan%20cs5a/Input.txt --output Input.txt
+ curl -s https://raw.githubusercontent.com/alanmathews9/s5-ss-lab/main/alan%20cs5a/Intermediate.txt --output Intermediate.txt
+ curl -s https://raw.githubusercontent.com/alanmathews9/s5-ss-lab/main/alan%20cs5a/Optab.txt --output Optab.txt
+ curl -s https://github.com/alanmathews9/s5-ss-lab/raw/main/alan%20cs5a/Symtab.txt --output Symtab.txt
+ echo "copied program to file pass_1.c"
+ echo "copied Input.txt to file Input.txt"
+ echo "copied Intermediate.txt to file Intermediate.txt"
+ echo "copied Optab.txt to file Optab.txt"
+ echo "copied Symtab.txt to file Symtab.txt"
+ echo "in the present working directory ( current folder )"
+ echo "CHANGE THE CONTENTS OF ALL TXT FILES ACCORDING TO THE QUESTION"
 elif [[ choice -eq 3 ]]
 then
-  curl -s https://raw.githubusercontent.com/nosomnesanates/CD_Lab_S7/master/Line_Count/line_count.lex --output line_count.lex
-    echo "line_count.lex"
+  curl -s https://raw.githubusercontent.com/alanmathews9/s5-ss-lab/main/alan%20cs5a/pass2.c --output pass_2.c
+  curl -s https://github.com/alanmathews9/s5-ss-lab/raw/main/alan%20cs5a/Output.txt--output Output.txt
+ curl -s https://raw.githubusercontent.com/alanmathews9/s5-ss-lab/main/alan%20cs5a/Intermediate.txt --output Intermediate.txt
+ curl -s https://raw.githubusercontent.com/alanmathews9/s5-ss-lab/main/alan%20cs5a/Optab.txt --output Optab.txt
+ curl -s https://github.com/alanmathews9/s5-ss-lab/raw/main/alan%20cs5a/Symtab.txt --output Symtab.txt 
+ curl -s https://github.com/alanmathews9/s5-ss-lab/raw/main/alan%20cs5a/Objcode.txt --output Objcode.txt 
+ 
+ echo "pass_2.c"
+ echo "copied Output.txt to file Output.txt"
+ echo "copied Intermediate.txt to file Intermediate.txt"
+ echo "copied Optab.txt to file Optab.txt"
+ echo "copied Symtab.txt to file Symtab.txt"
+ echo "in the present working directory ( current folder )"
+ echo "CHANGE THE CONTENTS OF ALL TXT FILES ACCORDING TO THE QUESTION"
 elif [[ choice -eq 4 ]]
 then
-  curl -s https://raw.githubusercontent.com/nosomnesanates/CD_Lab_S7/master/String_Operation/string_operation.lex --output string_operation.lex
-    echo "string_operation.lex"
+  curl -s https://raw.githubusercontent.com/alanmathews9/s5-ss-lab/main/alan%20cs5a/indexfile.c --output index_file_allocation.c
+    echo "index_file_allocation.c"
 elif [[ choice -eq 5 ]]
 then
-  curl -s https://raw.githubusercontent.com/nosomnesanates/Welp/main/compiler/cycle2/arith_exp.l --output arith_exp.lex
-    echo "arith_exp.lex"
+  curl -s https://raw.githubusercontent.com/alanmathews9/s5-ss-lab/main/alan%20cs5a/linkedfile.c --output linked_file_allocation.c
+    echo "linked_file_allocation.c.lex"
 elif [[ choice -eq 6 ]]
 then
-  curl -s https://raw.githubusercontent.com/nosomnesanates/Welp/main/compiler/cycle2/valid_id.l --output valid_id.l
-    echo "valid_id.l"
+  curl -s https://raw.githubusercontent.com/alanmathews9/s5-ss-lab/main/alan%20cs5a/seqfile2.c --output sequential_file_allocation.c
+    echo "sequential_file_allocation.c"
 elif [[ choice -eq 7 ]]
 then
-  curl -s https://raw.githubusercontent.com/nosomnesanates/Welp/main/compiler/cycle2/calc.l --output calc.l
-    echo "calc.l"
+  curl -s https://raw.githubusercontent.com/alanmathews9/s5-ss-lab/main/alan%20cs5a/paging.c --output paging.c
+    echo "paging.c"
 elif [[ choice -eq 8 ]]
 then
-  curl -s https://raw.githubusercontent.com/nosomnesanates/Welp/main/compiler/cycle2/syntaxtree.l --output syntaxtree.l
-    echo "syntaxtree.l"
+  curl -s https://raw.githubusercontent.com/alanmathews9/s5-ss-lab/main/alan%20cs5a/singlelevel.c --output single_level.c
+    echo "single_level.c"
 elif [[ choice -eq 9 ]]
 then
-  curl -s https://raw.githubusercontent.com/nosomnesanates/CD_Lab_S7/master/Epsilon_Closure/epsilon_closure.c --output epsilon_closure.c
-    echo "epsilon_closure.c"
+  curl -s https://raw.githubusercontent.com/alanmathews9/s5-ss-lab/main/alan%20cs5a/twolevel.c --output two_level.c
+    echo "two_level.c"
 elif [[ choice -eq 10 ]]
 then
-  curl -s https://raw.githubusercontent.com/nosomnesanates/CD_Lab_S7/master/Epsilon_remove/epsilon_remove.c --output epsilon_remove.c
-    echo "epsilon_remove.c"
-elif [[ choice -eq 11 ]]
-then
-  curl -s https://raw.githubusercontent.com/nosomnesanates/CD_Lab_S7/master/NFA_to_DFA/nfa_to_dfa.c --output nfa_to_dfa.c
-    echo "nfa_to_dfa.c"
-elif [[ choice -eq 12 ]]
-then
-  curl -s https://raw.githubusercontent.com/nosomnesanates/CD_Lab_S7/master/Minimize_DFA/minimize_dfa.c --output minimize_dfa.c
-    echo "minimize_dfa.c"
-elif [[ choice -eq 13 ]]
-then
-  curl -s https://raw.githubusercontent.com/nosomnesanates/CD_Lab_S7/master/First_And_Follow/first_and_follow.c --output first_and_follow.c
-    echo "first_and_follow.c"
-elif [[ choice -eq 14 ]]
-then
-  curl -s https://raw.githubusercontent.com/nosomnesanates/CD_Lab_S7/master/Recursive_Descent_Parser/recursive_descent.c --output recursive_descent.c
-    echo "recursive_descent.c"
-elif [[ choice -eq 15 ]]
-then
-  curl -s https://raw.githubusercontent.com/nosomnesanates/CD_Lab_S7/master/Shift_Reduce_Parser/shift_reduce.c --output shift_reduce.c
-    echo "shift_reduce.c"
-elif [[ choice -eq 16 ]]
-then
-  curl -s https://raw.githubusercontent.com/nosomnesanates/CD_Lab_S7/master/Constant_Propagation/constant_propagation.c --output constant_propagation.c
-    echo "constant_propagation.c"
-elif [[ choice -eq 17 ]]
-then
-  curl -s https://raw.githubusercontent.com/nosomnesanates/CD_Lab_S7/master/Intermediate_Code_Generation/inter_code.c --output inter_code.c
-    echo "inter_code.c"
-elif [[ choice -eq 18 ]]
-then
-  curl -s https://raw.githubusercontent.com/nosomnesanates/CD_Lab_S7/master/Three_Address_To_Assembly/three_addr_to_assm.c --output three_addr_to_assm.c
-    echo "three_addr_to_assm.c"
-elif [[ choice -eq 19 ]]
-then
-  curl -s https://raw.githubusercontent.com/nosomnesanates/CD_Lab_S7/master/Vowels/vowels.l --output vowels.l
-    echo "vowels.l"
+  curl -s https://raw.githubusercontent.com/alanmathews9/s5-ss-lab/main/alan%20cs5a/loaderpass1.c --output loader_pass_1.c
+    echo "loader_pass_1.c"
 else 
   echo "Invalid: $choice"
 fi
+echo "Type  history -c to clear history "
+echo "Type history -w to wipe your trace "
+echo "Thank me later by buying me an icecream :) "
 #clear
